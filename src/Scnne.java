5c994444e3db871c944f0a692e8a00f697b40835
Samuel Cavalcante <samuelcavalcantety@gmail.con>
2024-09-03 01:43:59 -0300

    ano de nascimento

--- src/Scnne.java
+++ src/Scnne.java
@@ -10,5 +10,7 @@ public class Scnne {
         Scanner teclado = new Scanner(System.in);
          System.out.print("Digite o seu nome:");
         String nome = teclado.nextLine();
+        System.out.print("Digite o ano de nascimento: ");
+        int anoNascimento = teclado.nextInt();
     }
 }
