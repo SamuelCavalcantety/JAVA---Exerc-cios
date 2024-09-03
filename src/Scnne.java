6439236292509f80beb810fbbd16afe4ed138673
Samuel Cavalcante <samuelcavalcantety@gmail.con>
2024-09-03 01:33:10 -0300

    Scanner

---
+++ src/Scnne.java
@@ -0,0 +1,14 @@
+
+
+// ENTRADA DE DADO (SCANNER) //
+
+import java.util.Scanner;
+
+public class Scnne {
+    public static void main (String[] args)
+    {
+        Scanner teclado = new Scanner(System.in);
+         System.out.print("Digite o seu nome:");
+                String nome = teclado.nextLine();
+    }
+}
